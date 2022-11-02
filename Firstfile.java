44ed9dbc5781625b8b9fe29d39e242da2ae60428
vaishnavi <sablevaishnavi07@gmail.com>
2022-11-02 17:25:39 +0530

    Code executed

--- Firstfile.java
+++ Firstfile.java
@@ -1,9 +1,20 @@
 public class first{
 
+<<<<<<< Updated upstream
 public void add(){
 
 System.out.println("Added method");
 }
 
+=======
+public static void main(String[] args) {
+
+int a=10;
+int b=20;
+int c=a+b;
+
+System.out.println(c);
+}
+>>>>>>> Stashed changes
 
 }
